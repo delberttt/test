73d4f073c3bbb9fdea084f4392e701a57a831a48
Clemence Goh <clemencegoh@users.noreply.github.com>
2018-10-09 14:54:44 +0800

    remove socket layer connection from miner

--- Exercises/week3/miner.py
+++ Exercises/week3/miner.py
@@ -1,9 +1,4 @@
 from Exercises.week1.keyPair import GenerateKeyPair
-from multiprocessing import Queue
-import sys
-import json
-import socket
-
 
 
 # todo: Idea is for every miner to have its own flask app
@@ -22,7 +17,6 @@ class Miner:
         self.tx_list = []
         self.balance = 0
         self.address = _public_key
-        self.connected_nodes = []
 
     @classmethod
     def new(cls, _public_key=None, _blockchain=None):
@@ -36,48 +30,11 @@ class Miner:
         # returns obj, public key and private key.
         return m, pub_key, priv_kev
 
-    def addPeers(self, _peers):
-        for peer in _peers:
-            if peer not in self.connected_nodes:
-                self.connected_nodes.append(peer)
-
-    # connectionStart is meant to be used to "start" the Miner
-    # every miner should be a server
-    def connectionStart(self, _port):
-        # create socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        server_address = ('localhost', _port)
-        print("Starting up miner server on {}...".format("localhost:", _port))
-        sock.bind(server_address)
-
-        # create queue todo: decide size of queue
-        blockchain_queue = Queue(10)
-        transaction_queue = Queue(10)
-
-        while True:
-            data, addrs = sock.recvfrom(4096)
-            data = json.loads(data.decode())
-
-            # todo: parse broadcast data
-
-    def broadcastTransaction(self, _tx):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        data = json.dumps({
-            "Type": "Transaction",
-            "Data": str(_tx)
-        })
-
-
-        
-
-    def broadcastBlock(self, _block):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        data = json.dumps({
-            "Type": "Block",
-            "Data": ""
-        })
+    def broadcast(self, _type, _data):
+        if _type == "Block":
+            pass
+        elif _type == "Transaction":
+            pass
 
 
 if __name__ == '__main__':
